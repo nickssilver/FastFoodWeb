dea15a2e1dce81205a3a02e445002cb972399bca
nickssilver <silvernicks9@gmail.com>
2023-09-30 00:42:58 +0300

    Add project files.

---
+++ FastFood.Models/ApplicationUser.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastFood.Models
+{
+    public class ApplicationUser : IdentityUser
+    { 
+        public string Name { get; set; }
+
+        public String City { get; set; }
+        public String Adresss { get; set; }
+
+        public string PostalCode { get; set; }
+
+    }
+}
---
+++ FastFood.Models/Cart.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastFood.Models
+{
+    public class Cart
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+        public int ItemId { get; set; }
+
+        public Item Item { get; set; }
+
+        public String ApplicationUserId { get; set; }
+
+        public ApplicationUser ApplicationUser { get; set; }
+        [Required, MinLength]
+
+        public int Count { get; set; }  
+    }
+}
